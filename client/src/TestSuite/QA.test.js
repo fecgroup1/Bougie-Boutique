d157f5682658d95d1b0fab60e2c7c0ac6576ccc2
Jackson <Jackson.harris3@gmail.com>
2021-04-17 18:34:59 -0700

    fix typo

--- client/src/TestSuite/QA.test.js
+++ client/src/TestSuite/QA.test.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { render, screen } from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect'
 
-import Appstore from '../Utils/Appstore'
+import Appstore from '../Utils/AppStore'
 import QandA from '../components/QandA'
 
 test('discretely renders QA to the DOM with Appstore state', () => {
