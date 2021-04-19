9100d04bdf0a292f052763374a83674ebd6b3d40
NoelleDeAngelo <NoelleDeAngelo@gmail.com>
2021-04-19 09:48:22 -0700

    Add route to reviewrouter for /meta

--- server/controllers/Review.js
+++ server/controllers/Review.js
@@ -1,72 +1,63 @@
 const url = 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-sjo/';
-const TOKEN = process.env.TOKEN || require('../../config.js');
 const axios = require('axios');
+const TOKEN = process.env.TOKEN || require('../../config.js');
 
-
-axios.defaults.headers.common['Authorization'] = TOKEN;
+axios.defaults.headers.common.Authorization = TOKEN;
 
 module.exports = {
 
   getMeta: (productId) => {
-    var  calculateAverage = module.exports.calculateAverage;
+    const { calculateAverage } = module.exports;
     return axios.get(`${url}reviews/meta?product_id=${productId}`)
+      .then((response) => {
+        const metaData = {
+          ratings: response.data.ratings,
+          recommended: response.data.recommended,
+          characteristics: response.data.characteristics,
+          averageRating: calculateAverage(response.data.ratings),
+          starRating: Math.round(4 * calculateAverage(response.data.ratings)) / 4,
+        };
+        return metaData;
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  },
+
+  calculateAverage: (ratingsObj) => {
+    let total = 0;
+    let count = 0;
+    for (key in ratingsObj) {
+      total += Number(ratingsObj[key]) * Number(key);
+      count += Number(ratingsObj[key]);
+    }
+    const average = total / count;
+    return Math.round(10 * average) / 10;
+  },
+
+  getReviews: (productId) => axios.get(`${url}reviews?product_id=${productId}`)
     .then((response) => {
-      let metaData = {
-        ratings: response.data.ratings,
-        recommended: response.data.recommended,
-        characteristics: response.data.characteristics,
-        averageRating: calculateAverage(response.data.ratings),
-        starRating: Math.round( 4 * calculateAverage(response.data.ratings)) /4
+      const reviews = [];
+      const listOfReviews = response.data.results;
+      for (let i = 0; i < listOfReviews.length; i++) {
+        const currentReview = listOfReviews[i];
+        reviews.push({
+          rating: currentReview.rating,
+          date: currentReview.date,
+          summary: currentReview.summary,
+          body: currentReview.body,
+          photos: currentReview.photos,
+          recommended: currentReview.recommend,
+          reviewerName: currentReview.reviewer_name,
+          responceToReview: currentReview.responce,
+          helpfulness: currentReview.helpfulness,
+        });
       }
-      return metaData;
+      return reviews;
     })
     .catch((err) => {
       console.log(err);
-    })
-  },
+    }),
+};
 
-
-  calculateAverage:(ratingsObj)=> {
-    var total= 0;
-    var count = 0;
-    for (key in ratingsObj){
-      total +=  Number(ratingsObj[key]) * Number(key)
-      count +=  Number(ratingsObj[key])
-    }
-    var average= total/count
-    return Math.round( 10* average) /10;
-  },
-
-  getReviews: (productId)=> {
-    return axios.get(`${url}reviews?product_id=${productId}`)
-      .then((response) => {
-        let reviews = [];
-        var listOfReviews= response.data.results;
-        for (var i=0; i< listOfReviews.length; i++){
-          var currentReview = listOfReviews[i];
-          reviews.push({
-            rating:currentReview.rating,
-            date:currentReview.date,
-            summary: currentReview.summary,
-            body: currentReview.body,
-            photos: currentReview.photos,
-            recommended: currentReview.recommend,
-            reviewerName: currentReview.reviewer_name,
-            responceToReview: currentReview.responce,
-            helpfulness: currentReview.helpfulness
-          })
-        }
-        return reviews;
-    })
-    .catch((err)=> {
-      console.log(err)
-    })
-  }
-}
-
-
-//module.exports.getReviews('13023').then((data)=> console.log('this is reviews', data))
-
-
-
-
+// module.exports.getReviews('13023').then((data)=> console.log('this is reviews', data))
--- server/routers/reviewRouter.js
+++ server/routers/reviewRouter.js
@@ -1,12 +1,18 @@
 const router = require('express').Router();
-const controllers = require('./../controllers');
+const controllers = require('../controllers');
 
- router.route('/')
- .get((req, res)=>{
-   controllers.review.getReviews(req.query.product_id)
-   .then((data)=> res.send(data))
-   .catch((err)=>res.end())
- })
+router.route('/')
+  .get((req, res) => {
+    controllers.review.getReviews(req.query.product_id)
+      .then((data) => res.send(data))
+      .catch((err) => res.end(err));
+  });
 
+router.route('/meta')
+  .get((req, res) => {
+    controllers.review.getMeta(req.query.product_id)
+      .then((data) => res.send(data))
+      .catch((err) => res.end(err));
+  });
 
- module.exports = router;
+module.exports = router;
