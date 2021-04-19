213e66e0cc15c2747f52ed8c7b04e48cdb482cf7
NoelleDeAngelo <NoelleDeAngelo@gmail.com>
2021-04-19 12:47:41 -0700

    Change axios call in ReviewAPI to return promise

--- client/src/Utils/ReviewAPI.js
+++ client/src/Utils/ReviewAPI.js
@@ -1,21 +1,14 @@
 import axios from 'axios';
 
-
-
 const ReviewAPI = {
-  getReviews: (product_id) {
-    axios.get(`/reviews?product_id=${product_id}`)
-    . then((data)=> data)
-    .catch((err)=> err)
+  getReviews: (product_id)=> {
+    axios.get(`/reviews?product_id=${product_id}`);
   },
 
-  getMeta:(product_id) {
-    axios.get(`/reviews/meta?product_id=${product_id}`)
-    . then((data)=> data)
-    .catch((err)=> err)
+  getMeta:(product_id)=> {
+    axios.get(`/reviews/meta?product_id=${product_id}`);
   }
 };
 
 export default ReviewAPI;
 
-console.log(ReviewAPI.getMeta('13023'))
