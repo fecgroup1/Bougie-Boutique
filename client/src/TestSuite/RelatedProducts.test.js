435a72ec81b6b03a4136f633919c8162c8beae9b
Jackson <Jackson.harris3@gmail.com>
2021-04-19 09:50:47 -0700

    update tests to match new naming conventions so that they pass circle ci

--- client/src/TestSuite/RelatedProducts.test.js
+++ client/src/TestSuite/RelatedProducts.test.js
@@ -3,12 +3,12 @@ import { render, screen } from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect'
 
 import RelatedProducts from '../components/RelatedProducts'
-import AppStore from '../Utils/AppStore'
+import CurrentProduct from '../Utils/CurrentProduct'
 
 describe('RelatedProducts', () => {
   test('renders the RelatedProducts component and allows for rendering of AppStore data', () => {
     render(
-      <RelatedProducts store={AppStore}/>
+      <RelatedProducts store={CurrentProduct}/>
     );
   })
 })
