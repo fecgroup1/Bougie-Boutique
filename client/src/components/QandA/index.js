b058761d50f1cfb41bcd1bbd59ae37fe0b9af9c1
Sidney <sid.nguyen@gmail.com>
2021-04-19 13:23:04 -0700

    rendering 2 questions on the QA widget

--- client/src/components/QandA/index.js
+++ client/src/components/QandA/index.js
@@ -1,9 +1,15 @@
 import React from 'react'
 import WidgetContainer from '../../Styles'
-import Question from './Question.js'
+import Questions from './Questions.js'
 
 const QandA = ({store}) => (
+<<<<<<< HEAD
   <div>I am a Question thing{store.state.currentProductId}</div>
+=======
+  <div>I am a Question thing {store.currentProductId}
+    <Questions currentProductId={store.currentProductId}/>
+  </div>
+>>>>>>> rendering 2 questions on the QA widget
   )
 
 export default QandA;
--- client/src/components/RatingsReviews/index.js
+++ client/src/components/RatingsReviews/index.js
@@ -2,7 +2,11 @@ import React from 'react'
 import WidgetContainer from '../../Styles'
 
 const Reviews = ({store}) => (
+<<<<<<< HEAD
   <div>I am a Reviews thing {store.state.currentProductId}</div>
+=======
+  <div>I am a Reviews thing {store.currentProductId}</div>
+>>>>>>> rendering 2 questions on the QA widget
 )
 
 export default Reviews;
