f6d0c11ebcdb76d9323c4377c650b9f7c8ffdfb0
Sidney <sid.nguyen@gmail.com>
2021-04-19 16:32:56 -0700

    resolved conflicts

--- client/src/components/QandA/index.js
+++ client/src/components/QandA/index.js
@@ -3,13 +3,9 @@ import WidgetContainer from '../../Styles'
 import Questions from './Questions.js'
 
 const QandA = ({store}) => (
-<<<<<<< HEAD
-  <div>I am a Question thing{store.state.currentProductId}</div>
-=======
   <div>I am a Question thing {store.currentProductId}
     <Questions currentProductId={store.currentProductId}/>
   </div>
->>>>>>> rendering 2 questions on the QA widget
   )
 
 export default QandA;
--- client/src/components/RatingsReviews/index.js
+++ client/src/components/RatingsReviews/index.js
@@ -2,11 +2,7 @@ import React from 'react'
 import WidgetContainer from '../../Styles'
 
 const Reviews = ({store}) => (
-<<<<<<< HEAD
   <div>I am a Reviews thing {store.state.currentProductId}</div>
-=======
-  <div>I am a Reviews thing {store.currentProductId}</div>
->>>>>>> rendering 2 questions on the QA widget
 )
 
 export default Reviews;
