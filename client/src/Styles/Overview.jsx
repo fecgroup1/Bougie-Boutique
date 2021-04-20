d4ecd055b675f7352d5a0bd7e8a57df12a47a8a4
Ting-Yu Chen <tingyuchen@Ting-Yus-MacBook-Pro.local>
2021-04-20 14:48:05 -0700

    Added styled component variables

--- client/src/Styles/Overview.jsx
+++ client/src/Styles/Overview.jsx
@@ -1,10 +1,15 @@
 import styled from 'styled-components';
 
+<<<<<<< HEAD
 export const StyleThumbs = styled.div`
+=======
+export const StyleThumbs = styled.img`
+>>>>>>> Added styled component variables
   display: grid;
   grid-template-columns: 1fr 1fr 1fr 1fr;
 `;
 
+<<<<<<< HEAD
 export const ThumbImg = styled.img`
   & {
     object-fit: cover;
@@ -29,6 +34,10 @@ export const CurrThumb = styled.img`
   margin: 6px;
   border: 2px solid ${props => props.theme.border};
   outline: 4px solid ${props => props.theme.outline};
+=======
+export const Stars = styled.div`
+  margin-bottom: 10px;
+>>>>>>> Added styled component variables
 `;
 
 export const Small = styled.span`
@@ -43,7 +52,11 @@ export const OuterStars = styled.div`
     color: ${props => props.theme.outline};
   }
   &:before {
+<<<<<<< HEAD
     content: "\f006  \f006  \f006  \f006  \f006";
+=======
+    content: "\f006 \f006 \f006 \f006 \f006";
+>>>>>>> Added styled component variables
   }
 `;
 export const InnerStars = styled.div`
@@ -55,11 +68,22 @@ export const InnerStars = styled.div`
     overflow: hidden;
   }
   &:before {
+<<<<<<< HEAD
     content: "\f005  \f005  \f005  \f005  \f005";
+=======
+    content: "\f005 \f005 \f005 \f005 \f005";
+>>>>>>> Added styled component variables
     color: ${props => props.theme.outline}
   }
 `;
 
+<<<<<<< HEAD
+=======
+export const Price = styled.span`
+  margin: 5px 0px;
+`;
+
+>>>>>>> Added styled component variables
 export const StyleName = styled.div`
   margin-top: 10px;
 `;
@@ -68,20 +92,38 @@ export const Flex = styled.div`
   display: flex;
   flex-wrap: wrap;
   justify-content: space-between;
+<<<<<<< HEAD
   margin: -10px;
+=======
+`;
+
+export const GalleryStyle = styled.div`
+  height: 500px;
+>>>>>>> Added styled component variables
 `;
 
 export const Left = styled.div`
   flex: 5 0 200px;
   order: 1;
+<<<<<<< HEAD
   margin: 10px;
+=======
+  margin-right: 20px;
+>>>>>>> Added styled component variables
 `;
 
 export const Right = styled.div`
   min-width: 400px;
   flex: 1 0 400px;
   order: 2;
+<<<<<<< HEAD
   margin: 10px;
+=======
+`;
+
+export const DetailStyling = styled.div`
+  padding-top: 10px;
+>>>>>>> Added styled component variables
 `;
 
 export const LinedTop = styled.div`
