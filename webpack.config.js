5edbafc357908eba0a0380d25d5ebe7814bda7a6
Sidney <sid.nguyen@gmail.com>
2021-04-20 11:36:09 -0700

    Decoupled babel presets in webpack from json

--- webpack.config.js
+++ webpack.config.js
@@ -20,10 +20,6 @@ module.exports = {
         use: {
           loader: 'babel-loader',
           options: {
-            presets: [
-              "@babel/preset-env",
-              "@babel/preset-react"
-            ],
             plugins: [
               [
                 "@babel/plugin-transform-runtime",
