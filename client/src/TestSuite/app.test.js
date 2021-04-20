ef82aae3b78cd5149497dd4332e1fa9de1dceee7
Ting-Yu Chen <tingyuchen@Ting-Yus-MacBook-Pro.local>
2021-04-20 12:18:37 -0700

    Some small cleanup

--- client/src/TestSuite/app.test.js
+++ client/src/TestSuite/app.test.js
@@ -2,7 +2,6 @@ import React from 'react'
 import { render, screen } from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect'
 
-// import App from './app.jsx'
 import App from '../app.jsx'
 
 describe('App', () => {
