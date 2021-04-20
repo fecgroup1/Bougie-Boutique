598a2917956f2344092d704d37264c7435c1fb64
NoelleDeAngelo <NoelleDeAngelo@gmail.com>
2021-04-20 14:38:51 -0700

    Add styled component for ratings

---
+++ client/src/Styles/RatingsandReviews.js
@@ -0,0 +1,5 @@
+import styled from 'styled-components'
+
+export const RatingsContainer = styled.div`
+  justify-content: center;
+`;
--- client/src/components/RatingsReviews/index.jsx
+++ client/src/components/RatingsReviews/index.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 // import WidgetContainer from '../../Styles'
 import Ratings from './Ratings';
 import Reviews from './Reviews';
+import { RatingsContainer } from '../../Styles'
 
 
 
@@ -14,9 +15,13 @@ class RatingsReviews extends React.Component {
   render(){
     return (<div>
       <h4>Ratings and Reviews</h4>
-      <Ratings meta={this.props.store.state.meta} />
+      <RatingsContainer>
+        <Ratings meta={this.props.store.state.meta} />
+      </RatingsContainer>
       <Reviews reviews ={this.props.store.state.reviews}/>
-    </div>);
+    </div>
+
+    );
 }
 }
 
