02fb5f448dc55c568e59b0aa81619960754a5dc9
Ting-Yu Chen <tingyuchen@Ting-Yus-MacBook-Pro.local>
2021-04-19 17:40:39 -0700

    Fixed price display

--- client/src/components/Overview/Styles/Select/Price.js
+++ client/src/components/Overview/Styles/Select/Price.js
@@ -8,16 +8,16 @@ const strikethru = {
   textDecoration: 'line-through'
 };
 
-const Price = ({orig, sale, noSale}) => {
-  if (noSale) {
+const Price = ({orig, sale}) => {
+  if (sale === null) {
     return(
       <span>{orig}</span>
     );
   } else {
     return(
       <>
-      <span style={salePrice}>{sale} </span>
-      <span style={strikethru}>{orig}</span>
+      <span style={salePrice}>${sale} </span>
+      <span style={strikethru}>${orig}</span>
       </>
     );
   }
