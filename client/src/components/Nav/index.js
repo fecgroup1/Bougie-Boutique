1668a7c14b688d9c1917dcde722d86813e79491b
Ting-Yu Chen <tingyuchen@Ting-Yus-MacBook-Pro.local>
2021-04-20 19:41:45 -0700

    Refactored to stateless comp

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,59 +1,50 @@
 import React from 'react';
 import { Title, ThemeToggle } from './../../Styles';
 
-class Nav extends React.PureComponent {
-  constructor(props) {
-    super(props);
+const Nav = ({dark, toggleTheme}) => {
+  const header = {
+    width: '100%',
+    background: '#002a60',
+    color: '#cccccc',
+    height: '80',
+    lineHeight: '80px',
+    marginBottom: '20px',
   }
 
-  render() {
-    const dark = this.props.dark;
-    const toggleTheme = this.props.toggleTheme;
-
-    const header = {
-      width: '100%',
-      background: '#002a60',
-      color: '#cccccc',
-      height: '80',
-      lineHeight: '80px',
-      marginBottom: '20px',
-    }
-
-    const title = {
-      position: 'relative',
-      paddingLeft: '20px'
-    }
-
-    const right = {
-      position: 'absolute',
-      top: '24px',
-      right: '20px',
-    }
-
-    const label = {
-      position: 'absolute',
-      fontFamily:"'Josefin Sans', sans-serif",
-      fontSize: '12px',
-      top: '17px',
-      right: '20px',
-    }
-
-    const icon = dark ? 'lni lni-night': 'lni lni-sun'
-
-    return(
-      <div id="top" style={header}>
-        <Title style={title}>
-          Bougie Boutique
-        </Title>
-        <div style={label}>Theme</div>
-          <ThemeToggle
-            style={right}
-            onClick={toggleTheme}>
-              <span className={icon}></span>
-          </ThemeToggle>
-      </div>
-    );
+  const title = {
+    position: 'relative',
+    paddingLeft: '20px'
   }
+
+  const right = {
+    position: 'absolute',
+    top: '24px',
+    right: '20px',
+  }
+
+  const label = {
+    position: 'absolute',
+    fontFamily:"'Josefin Sans', sans-serif",
+    fontSize: '12px',
+    top: '17px',
+    right: '20px',
+  }
+
+  const icon = dark ? 'lni lni-night': 'lni lni-sun'
+
+  return(
+    <div id="top" style={header}>
+      <Title style={title}>
+        Bougie Boutique
+      </Title>
+      <div style={label}>Theme</div>
+        <ThemeToggle
+          style={right}
+          onClick={toggleTheme}>
+            <span className={icon}></span>
+        </ThemeToggle>
+    </div>
+  );
 }
 
 export default Nav;
