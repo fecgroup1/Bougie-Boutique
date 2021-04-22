58929077777b03a39dc4ee48924a07ffd95a19ca
NoelleDeAngelo <NoelleDeAngelo@gmail.com>
2021-04-21 19:48:22 -0700

    Add submit button to new review form

--- client/src/components/RatingsReviews/NewReviewModal.jsx
+++ client/src/components/RatingsReviews/NewReviewModal.jsx
@@ -20,10 +20,10 @@ const AddCharRateing = (Characteristic, value) => {
 
 const generateCharacteristic = (Characteristic)=> {
   return (
-    <div style={{'marginTop': '25px'}}><br></br>
+    <div style={{'marginTop': '15px'}}><br></br>
       <span> * </span><u>{Characteristic}:</u> <span  style= {{'fontSize': '90%'}} id= {Characteristic}></span><br></br><br></br>
       <label style= {{'fontSize': '85%'}} >{CharacteristicDeff[Characteristic][0]} </label>
-      <input onClick= {(event)=> AddCharRateing( Characteristic, event.target.value) } type="radio" name={Characteristic} value="1" ></input>
+      <input required onClick= {(event)=> AddCharRateing( Characteristic, event.target.value) } type="radio" name={Characteristic} value="1" ></input>
       <input onClick= {(event)=> AddCharRateing( Characteristic, event.target.value) } type="radio" name={Characteristic} value="2" ></input>
       <input onClick= {(event)=> AddCharRateing( Characteristic, event.target.value) } type="radio" name={Characteristic} value="3" ></input>
       <input onClick= {(event)=> AddCharRateing( Characteristic, event.target.value) } type="radio" name={Characteristic} value="4" ></input>
@@ -44,7 +44,7 @@ const NewReviewModal = ({close, isOpen, productName, characteristics})=> {
     <form >
       <label style={{'marginTop': '40px'}} for='starRating'>* Overall Rating:  </label>
       <span id= 'starRating'>
-        <input type="radio" name="overallRating" value="1"></input>
+        <input required type="radio" name="overallRating" value="1"></input>
         <input type="radio" name="overallRating" value="2"></input>
         <input type="radio" name="overallRating" value="3"></input>
         <input type="radio" name="overallRating" value="4"></input>
@@ -55,9 +55,20 @@ const NewReviewModal = ({close, isOpen, productName, characteristics})=> {
       <label for="yes">Yes</label><br></br>
       <input type="radio" name="recomended" value="No" id= 'no'></input>
       <label for="no">No</label>
-
       {Object.keys(characteristics).map((key)=> generateCharacteristic(key))}
-
+      <div style={{'marginTop': '40px'}} > Review Summary:  </div><br></br>
+      <input type='text' size='50' maxLength= '60' id= 'summary' placeholder= 'Example: Best purchase ever!'></input>
+      <div style={{'marginTop': '40px'}} >* Review:  </div><br></br>
+      <textarea required minlength= '50' maxLength='1000'  rows='4' cols ='50'placeholder='Why did you like the product or not?'></textarea>
+      <div style={{'marginTop': '40px', 'marginBottom': '15px'}}> Upload your photos</div>
+      <input type= 'file' accept="image/png, image/jpeg"></input>
+      <div style={{'marginTop': '40px'}} >* What is your nickname?</div><br></br>
+      <input  required type='text' size='50' maxLength= '60' id= 'userName' placeholder= 'jackson11!'></input>
+      <div style= {{'marginTop': '10px','fontSize': '77%'}}>For privacy reasons, do not use your full name or email address.</div>
+      <div style={{'marginTop': '40px'}} >* What is your email?</div><br></br>
+      <input  required type='email' size='50' maxLength= '60' id= 'email' placeholder= 'jackson11@email.com'></input>
+      <div style= {{'marginTop': '10px','fontSize': '77%'}}>For authentication reasons, you will not be emailed.</div>
+      <button  style={{'marginTop': '15px'}} type= 'submit'>Submit Review</button>
     </form>
   </Modal>
   )
