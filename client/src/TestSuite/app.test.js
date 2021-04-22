942617f9650122c2f9da6a0b4ebd6fc9ef7fa818
Jackson <Jackson.harris3@gmail.com>
2021-04-22 13:36:50 -0700

    more circle ci import adjustments

---
+++ client/src/TestSuite/app.test.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import 'isomorphic-fetch';
+
+import App from '../app.jsx'
+
+describe('App', () => {
+  test('renders the App component', () => {
+    render(<App />);
+  })
+})
