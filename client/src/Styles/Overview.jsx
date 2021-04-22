6a58caa49f04db7657716af0d9160dafd95a2c79
Ting-Yu Chen <tingyuchen@Ting-Yus-MacBook-Pro.local>
2021-04-22 09:55:58 -0700

    Moved overview select and button styling out of global styling

--- client/src/Styles/Overview.jsx
+++ client/src/Styles/Overview.jsx
@@ -88,3 +88,32 @@ export const LinedTop = styled.div`
   margin-top: 10px;
   padding-top: 10px;
 `;
+
+export const AddToCartButton = styled.button`
+  & {
+    border: 3px solid ${props => props.theme.text};
+    color: ${props => props.theme.text};
+    background: ${props => props.theme.bg};
+    outline: #002a60;
+    transition: background 0.5s;
+    padding: 1em;
+    text-align: center;
+    height: 4em;
+    font-size: 1em;
+    font-family: 'Josefin Sans', sans-serif;
+  }
+  &: hover {
+    background: #002a60;
+  }
+`;
+
+export const CartDropdown = styled.select`
+  border: 3px solid ${props => props.theme.text};
+  color: ${props => props.theme.text};
+  background: ${props => props.theme.bg};
+  outline: #0056c5;
+  height: 4em;
+  padding: 1em 0em;
+  font-family: 'Josefin Sans', sans-serif;
+  font-size: 1em;
+`;
