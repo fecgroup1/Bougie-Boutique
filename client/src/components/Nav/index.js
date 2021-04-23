0620236ac59c71a998e667459eaf493ba8f5304a
Ting-Yu Chen <tingyuchen@Ting-Yus-MacBook-Pro.local>
2021-04-23 08:47:37 -0700

    Added hover toggle effect

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -9,118 +9,127 @@ const countItems = (obj) => {
   return count;
 }
 
-const Nav = ({cart, store, checkCart, dark, toggleTheme}) => {
-
-  checkCart(store);
-
-  const cartCount = countItems(cart);
-
-  const header = {
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'space-between',
-    width: '100%',
-    background: '#002a60',
-    color: '#cccccc',
-    height: '80px',
-    marginBottom: '20px',
+class Nav extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      translate: this.props.dark ? -0.95: 0
+    }
+    this.handleMouseOver = this.handleMouseOver.bind(this);
+    this.handleMouseLeave = this.handleMouseLeave.bind(this);
   }
 
-  const title = {
-    position: 'relative',
-    paddingLeft: '20px'
+  handleMouseOver() {
+    this.setState({
+      translate: this.props.dark ? 0: -0.95
+    });
   }
 
-  const right = {
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'flex-end',
+  handleMouseLeave() {
+    this.setState({
+      translate: this.props.dark ? -0.95: -0
+    });
   }
 
-  const track = {
-    display: 'flex',
-    justifyContent: 'space-between',
-    alignContent: 'center',
-    background: `#${dark ? '001a3c':'003E8D'}`,
-    height: '1.5em',
-    width: '3em',
-    borderRadius: '25%/50%',
-    fontSize: '1.25em',
-    padding: '0.25em',
-    textAlign: 'justify',
-    marginLeft: '10px',
-    transition: 'background 0.5s',
-  }
+  render() {
+    this.props.checkCart(store);
 
-  const toggle = {
-    position: 'relative',
-    right: '30px',
-    transform: `translateX(${dark ? -0.95: 0}em)`,
-    transition: 'transform 0.5s',
-    background: '#ccc',
-    width: '0.75em',
-    height: '0.75em',
-    borderRadius: '50%',
-  }
+    const cart = this.props.cart;
+    const store = this.props.store;
+    const dark = this.props.dark;
+    const toggleTheme = this.props.toggleTheme;
 
-  const cartIcon = {
-    right: '70px',
-  }
-  const number = {
-    position: 'relative',
-    top: '0.25em',
-    left: '2em',
-    fontFamily:"'Josefin Sans', sans-serif",
-    fontSize: '0.75em',
-    textAlign: 'center',
-    width: '20px',
-  }
-  // const label = {
-  //   position: 'absolute',
-  //   fontFamily:"'Josefin Sans', sans-serif",
-  //   fontSize: '12px',
-  //   right: '20px',
-  //   textShadow:
-  //   `-1px -1px 0 #002a60,
-  //     1px -1px 0 #002a60,
-  //     -1px 1px 0 #002a60,
-  //     1px 1px 0 #002a60`,
-  //   zIndex: 1,
-  // }
+    const cartCount = countItems(cart);
 
-  const icon = dark ? 'lni lni-night': 'lni lni-sun'
+    const header = {
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'space-between',
+      width: '100%',
+      background: '#002a60',
+      color: '#cccccc',
+      height: '80px',
+      marginBottom: '20px',
+    }
 
-  return(
-    <div id="top" style={header}>
-      <Title style={title}>
-        Bougie Boutique
-      </Title>
-      <div className="headerRight" style={right}>
-        <div style={number}>{cartCount}</div>
-        <NavButton
-            style={cartIcon}>
-            <span className="lni lni-tshirt"></span>
-        </NavButton>
-        <NavButton
-          style={track}
-          onClick={toggleTheme}>
-            <span className="lni lni-sun"></span>
-            <span> </span>
-            <span className="lni lni-night"></span>
-        </NavButton>
-        <NavButton
-          style={toggle}
-          onClick={toggleTheme}>
-        </NavButton>
+    const title = {
+      position: 'relative',
+      paddingLeft: '20px'
+    }
+
+    const right = {
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'flex-end',
+    }
+
+    const track = {
+      display: 'flex',
+      justifyContent: 'space-between',
+      alignContent: 'center',
+      background: `#${dark ? '001a3c':'003E8D'}`,
+      height: '1.5em',
+      width: '3em',
+      borderRadius: '25%/50%',
+      fontSize: '1.25em',
+      padding: '0.25em',
+      textAlign: 'justify',
+      marginLeft: '10px',
+      transition: 'background 0.5s',
+    }
+
+    const toggle = {
+      position: 'relative',
+      right: '30px',
+      transform: `translateX(${this.state.translate}em)`,
+      transition: 'transform 0.5s',
+      background: '#ccc',
+      width: '0.75em',
+      height: '0.75em',
+      borderRadius: '50%',
+    }
+
+    const cartIcon = {
+      right: '70px',
+    }
+    const number = {
+      position: 'relative',
+      top: '0.25em',
+      left: '2em',
+      fontFamily:"'Josefin Sans', sans-serif",
+      fontSize: '0.75em',
+      textAlign: 'center',
+      width: '20px',
+    }
+    return(
+      <div id="top" style={header}>
+        <Title style={title}>
+          Bougie Boutique
+        </Title>
+        <div className="headerRight" style={right}>
+          <div style={number}>{cartCount}</div>
+          <NavButton
+              style={cartIcon}>
+              <span className="lni lni-tshirt"></span>
+          </NavButton>
+          <NavButton
+            style={track}
+            onClick={toggleTheme}
+            onMouseOver={this.handleMouseOver}
+            onMouseLeave={this.handleMouseLeave}>
+              <span className="lni lni-sun"></span>
+              <span> </span>
+              <span className="lni lni-night"></span>
+          </NavButton>
+          <NavButton
+            style={toggle}
+            onClick={toggleTheme}
+            onMouseOver={this.handleMouseOver}>
+          </NavButton>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
-
-{/* <div
-onClick={toggleTheme}
-style={label}>
-  Theme
-</div> */}
 export default Nav;
