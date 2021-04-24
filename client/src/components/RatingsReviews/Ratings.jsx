697f1946d3f0448f380637b9bdaa8eb739fb8aa5
NoelleDeAngelo <NoelleDeAngelo@gmail.com>
2021-04-23 17:36:47 -0700

    Added bar guages for characteristics

--- client/src/components/RatingsReviews/Ratings.jsx
+++ client/src/components/RatingsReviews/Ratings.jsx
@@ -34,12 +34,11 @@ const Ratings = ({ meta, filterReviews, filteredFor }) => {
             <StarsInner rating={meta.starRating}/>
     </StarsOuter>
     <p>{calculatePercent(meta.recommended)}% of reviews recomend this product</p>
-    {filteredFor['5']? <i>&#10003;</i> : <div></div>} <a onClick= {()=> filterReviews('5')}>5 stars</a><meter min= '0' max= '100' value= {meta.ratings['5']? (meta.ratings['5'] / totalReviews)* 100 : 0}></meter><br></br>
-    {filteredFor['4']? <i>&#10003;</i> : <div></div>}<a onClick= {()=> filterReviews('4')}>4 stars</a><meter min= '0' max= '100' value= {meta.ratings['4']? (meta.ratings['4'] / totalReviews)* 100 : 0}></meter><br></br>
-    {filteredFor['3']? <i>&#10003;</i> : <div></div>}<a onClick= {()=> filterReviews('3')}>3 stars</a><meter min= '0' max= '100' value= {meta.ratings['3']? (meta.ratings['3'] / totalReviews)* 100 : 0}></meter><br></br>
-    {filteredFor['2']? <i>&#10003;</i> : <div></div>}<a onClick= {()=> filterReviews('2')}>2 stars</a><meter min= '0' max= '100' value= {meta.ratings['2']? (meta.ratings['2'] / totalReviews)* 100 : 0}></meter><br></br>
-    {filteredFor['1']? <i>&#10003;</i> : <div></div>}<a onClick= {()=> filterReviews('1')}>1 stars</a><meter min= '0' max= '100' value= {meta.ratings['1']? (meta.ratings['1'] / totalReviews)* 100 : 0}></meter><br></br>
-
+        {filteredFor['5']? <i>&#10003;</i> : <div></div>} <a onClick= {()=> filterReviews('5')}>5 stars </a><meter min= '0' max= '100' value= {meta.ratings['5']? (meta.ratings['5'] / totalReviews)* 100 : 0}></meter><br></br>
+        {filteredFor['4']? <i>&#10003;</i> : <div></div>}<a onClick= {()=> filterReviews('4')}>4 stars </a><meter min= '0' max= '100' value= {meta.ratings['4']? (meta.ratings['4'] / totalReviews)* 100 : 0}></meter><br></br>
+        {filteredFor['3']? <i>&#10003;</i> : <div></div>}<a onClick= {()=> filterReviews('3')}>3 stars </a><meter min= '0' max= '100' value= {meta.ratings['3']? (meta.ratings['3'] / totalReviews)* 100 : 0}></meter><br></br>
+        {filteredFor['2']? <i>&#10003;</i> : <div></div>}<a onClick= {()=> filterReviews('2')}>2 stars </a><meter min= '0' max= '100' value= {meta.ratings['2']? (meta.ratings['2'] / totalReviews)* 100 : 0}></meter><br></br>
+        {filteredFor['1']? <i>&#10003;</i> : <div></div>}<a onClick= {()=> filterReviews('1')}>1 stars </a><meter min= '0' max= '100' value= {meta.ratings['1']? (meta.ratings['1'] / totalReviews)* 100 : 0}></meter><br></br><br></br>
     <Characteristics characteristics={meta.characteristics} />
   </div>);
 }
