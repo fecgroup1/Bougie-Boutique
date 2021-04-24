4edc2746a2bdc0829ce24e933f1a4f97b784864b
Ting-Yu Chen <tingyuchen@Ting-Yus-MacBook-Pro.local>
2021-04-24 09:58:05 -0700

    Added ExpandedView component

--- client/src/components/Overview/Gallery/DefaultView.js
+++ client/src/components/Overview/Gallery/DefaultView.js
@@ -1,24 +1,51 @@
 import React from 'react';
 import { Loading, MainImg, MainNull } from './../../../Styles/Overview';
 
-const DefaultView = ({styles, currImg}) => {
+class DefaultView extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state={
+      modalOpen: false,
+    }
+    this.handleModel = this.handleModel.bind(this);
+  }
 
-  if (styles[0].name === null) {
-  return (
-    <Loading
-      src={styles[currImg[0]].photos[currImg[1]].url} />
-  );
-  } else if ( styles[currImg[0]].photos[currImg[1]].url === null) {
+  handleModel(bool) {
+    this.setState({
+      modalOpen: bool,
+    });
+  }
+
+  render() {
+    const styles = this.props.styles;
+    const currImg = this.props.currImg;
+
+    if (styles[0].name === null) {
     return (
-      <MainNull
-        // Add onClick
-        src='https://lineicons.com/wp-content/themes/xt-lineicons/free-regular-icons/circle-minus.svg'/>
+      <Loading
+        src={styles[currImg[0]].photos[currImg[1]].url} />
+      <ExpandedView
+        isOpen={this.state.modalOpen}
+        handleModel={this.handleModel} />
     );
-  } else {
-    return (
-      <MainImg
-        src={styles[currImg[0]].photos[currImg[1]].url}/>
-    );
+    } else if ( styles[currImg[0]].photos[currImg[1]].url === null) {
+      return (
+        <MainNull
+          onClick={() => this.handleModel(true)}
+          src='https://lineicons.com/wp-content/themes/xt-lineicons/free-regular-icons/circle-minus.svg'/>
+        <ExpandedView
+          isOpen={this.state.modalOpen}
+          handleModel={this.handleModel} />
+      );
+    } else {
+      return (
+        <MainImg
+          src={styles[currImg[0]].photos[currImg[1]].url}/>
+        <ExpandedView
+          isOpen={this.state.modalOpen}
+          handleModel={this.handleModel} />
+      );
+    }
   }
 };
 
---
+++ client/src/components/Overview/Gallery/ExpandedView.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Modal from 'react-modal';
+
+import { Loading, MainImg, MainNull } from './../../../Styles/Overview';
+
+const ExpandedView = ({isOpen}) => {
+
+
+  return (
+    <Modal isOpen={isOpen}>
+
+    </Modal>
+  );
+};
+
+export default ExpandedView;
