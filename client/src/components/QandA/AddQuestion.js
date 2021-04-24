1dca4ec90067cbea15056ce50fc9198bddaae613
Sidney <sid.nguyen@gmail.com>
2021-04-24 09:38:31 -0700

    refactored question modal to react hooks

--- client/src/components/QandA/AddQuestion.js
+++ client/src/components/QandA/AddQuestion.js
@@ -1,42 +1,35 @@
-import React, {Fragment} from 'react';
+import React, { Fragment, useState, useEffect } from 'react';
 import axios from 'axios';
 import WidgetContainer from '../../Styles'
 import Modal from 'react-modal'
 
 
-//Will implement photos later
+const AddQuestion = (props) => {
+  const [open, setOpen] = useState(false);
+  const [chars, setChars] = useState(1000);
 
-class AddQuestion extends React.Component {
-  constructor(props) {
-    super(props)
+  useEffect(() => {
+    setOpen(false)
+    setChars(1000)
+  }, [props.currentProductId])
 
-    this.state = {
-      open: false,
-      chars: 1000
-    }
-    this.submitForm = this.submitForm.bind(this)
-    this.openModal = this.openModal.bind(this)
-    this.closeForm = this.closeForm.bind(this)
-    this.charsLeft = this.charsLeft.bind(this)
-  }
-
-  openModal(event) {
+  const openModal = (event) => {
     event.preventDefault()
-    this.setState({open: true})
+    setOpen(true)
     document.body.style.overflow = 'hidden';
   }
 
-  closeForm() {
-    this.setState({open: false})
+  const closeForm = () => {
+    setOpen(false)
     document.body.style.overflow = 'unset'
   }
 
-  charsLeft (e) {
+  const charsLeft = (e) => {
     const remaining = 1000 - e.target.value.length;
-    this.setState({chars: remaining })
+    setChars(remaining)
   }
 
-  submitForm(event) {
+  const submitForm = (event) => {
     event.preventDefault();
     let questionBody = document.getElementById('questionInputText')
     let nickname = document.getElementById('questionNickname')
@@ -44,7 +37,7 @@ class AddQuestion extends React.Component {
 
     if ((questionBody.validity.valid) && (nickname.validity.valid)
         && email.validity.valid) {
-      const pid = Number(this.props.currentProductId);
+      const pid = Number(props.currentProductId);
       axios.post(`/qa/questions`, {
         body: questionBody.value,
         name: nickname.value,
@@ -57,60 +50,59 @@ class AddQuestion extends React.Component {
       .catch((err) => {
         console.log(err)
       })
-      this.closeForm()
+      closeForm()
     } else {
       return;
     }
   }
 
-  render() {
-    const overlay = { overlay: {
-      backgroundColor: 'rgba(17, 17, 17, 0.75)'
-    }}
-    return (
-      <Fragment>
-        <Modal
-         ariaHideApp={false}
-         isOpen={this.state.open}
-         className='questionModal'
-         style={overlay}
-         overlayClassName={{
-           base: 'questionModalOverlay',
-           afterOpen: 'questionModalOverlay-in',
-           beforeClose: 'questionModalOverlay-out'
-         }}
-         onRequestClose={() => this.closeForm()}
-        >
-        <form>
-          <div>
-            <button id='closeModal' onClick={this.closeForm}>X</button>
-            <h2>Ask your question</h2>
-            <h4>About the Product</h4>
-            <label>* Your Question: </label>
-            <textarea id='questionInputText' name='question' type='text' onChange={(e) => {this.charsLeft(e)}} maxLength='1000' required />
-            <p id='charsLeft'>{this.state.chars} characters remaining</p>
+  const overlay = { overlay: {
+    backgroundColor: 'rgba(17, 17, 17, 0.75)'
+  }}
 
-            <label>* What is your nickname? </label>
-            <input type="text" id="questionNickname" className='modalInput'
-             placeholder='Example: jackson11!' name="nickname" maxLength='60' required></input>
-             <p className='warning'>For privacy reasons, do not use your full name or email address</p><br></br>
+  return (
+    <Fragment>
+      <Modal
+        ariaHideApp={false}
+        isOpen={open}
+        className='questionModal'
+        style={overlay}
+        overlayClassName={{
+          base: 'questionModalOverlay',
+          afterOpen: 'questionModalOverlay-in',
+          beforeClose: 'questionModalOverlay-out'
+        }}
+        onRequestClose={() => closeForm()}
+      >
+      <form>
+        <div>
+          <button id='closeModal' onClick={closeForm}>X</button>
+          <h2>Ask your question</h2>
+          <h4>About the Product</h4>
+          <label>* Your Question: </label>
+          <textarea id='questionInputText' name='question' type='text' onChange={(e) => {charsLeft(e)}} maxLength='1000' required />
+          <p id='charsLeft'>{chars} characters remaining</p>
 
-            <label>* Your email: </label>
-            <input type="email" id="questionEmail" className='modalInput'
-             placeholder='Why did you like the product or not?' name="email" maxLength='60' required></input>
-             <p className='warning'>For authentication reasons, you will not be emailed</p><br></br>
+          <label>* What is your nickname? </label>
+          <input type="text" id="questionNickname" className='modalInput'
+            placeholder='Example: jackson11!' name="nickname" maxLength='60' required></input>
+            <p className='warning'>For privacy reasons, do not use your full name or email address</p><br></br>
 
-            <p id='required'>* Required</p>
+          <label>* Your email: </label>
+          <input type="email" id="questionEmail" className='modalInput'
+            placeholder='Why did you like the product or not?' name="email" maxLength='60' required></input>
+            <p className='warning'>For authentication reasons, you will not be emailed</p><br></br>
 
-            <button id='qaSubmit' onClick={(event) => this.submitForm(event)}>Submit</button>
-          </div>
-        </form>
-        </Modal>
-        <button id='addQuestionButton' onClick={this.openModal}>ASK A QUESTION +</button>
-      </Fragment>
+          <p id='required'>* Required</p>
 
-    )
-  }
+          <button id='qaSubmit' onClick={(event) => submitForm(event)}>Submit</button>
+        </div>
+      </form>
+      </Modal>
+      <button id='addQuestionButton' onClick={openModal}>ASK A QUESTION +</button>
+    </Fragment>
+
+  )
 }
 
 export default AddQuestion
