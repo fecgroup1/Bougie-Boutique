054267f329746e9fa6d127a1186866c480259c44
Jackson <Jackson.harris3@gmail.com>
2021-04-24 20:59:14 -0700

    complete test for TrackingAPI

--- client/src/TestSuite/clickTracking.test.js
+++ client/src/TestSuite/clickTracking.test.js
@@ -1,2 +1,16 @@
-import TrackingAPI from '..Utils/TrackingAPI';
+import TrackingAPI from '../Utils/TrackingAPI';
 import axios from 'axios';
+
+jest.mock('axios');
+
+test('Post click tracking to interactions API',
+() => {
+  const click = { elemnt: '<h1> Hola Mundo </h1', time: '12th of never', widget: 'test land'}
+
+  const resp = {status: 200}
+   axios.post.mockResolvedValue(resp);
+
+   return TrackingAPI.sendTrackingData(click)
+   .then(data => expect(data.status).toEqual(200))
+}
+)
--- client/src/Utils/RelatedAPI.js
+++ client/src/Utils/RelatedAPI.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+
 const RelatedAPI = {
   getRelatedProducts: (pid) => {
     return axios.get(`/product/related?pid=${pid}`)
