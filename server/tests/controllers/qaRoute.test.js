3df08c86f069fd8fec12e59e9e1b322b06c65fc1
Jackson <Jackson.harris3@gmail.com>
2021-04-27 12:55:00 -0700

    prep for rebase

--- server/tests/controllers/qaRoute.test.js
+++ server/tests/controllers/qaRoute.test.js
@@ -10,6 +10,16 @@ describe('Get API Endpoints', () => {
       .expect(200)
       expect(res.body.product_id).toBe("13023")
   })
+
+  it('should get related products for a given product id', async (done) => {
+    const res = await request(app)
+      .get('/product/related/?pid=13030')
+      .expect("Content-Type", /json/)
+      .expect(200, done)
+      .end(function(err, res) {
+        if (err) throw err;
+      })
+  });
 })
 
 // describe('Post in API Endpoints', () => {
--- server/tests/routers/routers.test.js
+++ server/tests/routers/routers.test.js
@@ -2,22 +2,14 @@ const TOKEN = process.env.TOKEN
 const request = require('supertest')
 const app = require('../../index.js')
 
-describe('tests for routes', () => {
-  it('should get related products for a given product id', (done) => {
-    request(app)
+xdescribe('tests for routes', () => {
+  xit('should get related products for a given product id', async (done) => {
+    const res = await request(app)
       .get('/product/related/?pid=13030')
       .expect("Content-Type", /json/)
       .expect(200, done)
-      // expect(res.body.currentProductId).toBe(13024)
-      // console.log(res.body)
-      .end((err, res) => {
-        if (err) return done(err)
-        return done();
+      .end(function(err, res) {
+        if (err) throw err;
       })
-      // .then((res) => {
-
-      //   done();
-      // })
-      // .catch(err => done(err))
   });
 })
