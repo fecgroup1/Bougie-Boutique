d28a073ec7937e1e60377c090a45321f234a5bfd
Jackson <Jackson.harris3@gmail.com>
2021-04-27 15:53:19 -0700

    remove extra router export that is no longer required

--- server/routers/index.js
+++ server/routers/index.js
@@ -1,4 +1,3 @@
 exports.productRouter = require('./productRouter.js');
 exports.reviewRouter = require('./reviewRouter.js');
-exports.qaRouter = require('./qaRouter.js');
 exports.trackingRouter = require('./trackingRouter.js')
