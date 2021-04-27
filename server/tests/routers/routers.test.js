c36804a2b63fe77283fd26d85016e5e762915e5a
Jackson <Jackson.harris3@gmail.com>
2021-04-27 15:25:09 -0700

    refactor server routes tests into one file. create additional test

--- server/tests/routers/routers.test.js
+++ server/tests/routers/routers.test.js
@@ -2,14 +2,29 @@ const TOKEN = process.env.TOKEN
 const request = require('supertest')
 const app = require('../../index.js')
 
-xdescribe('tests for routes', () => {
-  xit('should get related products for a given product id', async (done) => {
-    const res = await request(app)
+describe('tests for routes', () => {
+  it('should get related products for a given product id', () => {
+    request(app)
       .get('/product/related/?pid=13030')
       .expect("Content-Type", /json/)
-      .expect(200, done)
-      .end(function(err, res) {
-        if (err) throw err;
+      .expect(200)
+      // console.log(res.body)
+      .then((res) => {
+        expect(res.body.currentProductId).toBe(13024)
       })
+      .catch(err => err)
   });
+
+  it('should get data from the API', () => {
+    request(app)
+      .get('/qa/questions?product_id=13023&count=50')
+      .expect("Content-Type", /json/)
+      .expect(200)
+      .then((res) => {
+        expect(res.body.product_id).toBe("13023")
+      })
+      .catch(err => err)
+  })
+
+
 })
