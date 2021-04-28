ed8f6e7e3f5d862f4aceea7f93eb5c98b02f1299
NoelleDeAngelo <NoelleDeAngelo@gmail.com>
2021-04-27 21:21:03 -0700

    add test for getreviews

---
+++ client/src/TestSuite/ReviewsAPI.test.jsx
@@ -0,0 +1,46 @@
+import ReviewAPI from '../Utils/ReviewAPI';
+import axios from 'axios'
+
+jest.mock('axios');
+
+test('getReviews gets the reviews the current product and returns it as an array of objects', () => {
+  var reviews= [
+    {
+        "rating": 5,
+        "date": "2021-03-08T00:00:00.000Z",
+        "summary": "Makes me smile",
+        "body": "This product is great because it puts a little smile on my face!",
+        "photos": [
+            {
+                "id": 496602,
+                "url": "https://i.ibb.co/5xbC4Xk/happy-Monkey.jpg"
+            }
+        ],
+        "recommended": true,
+        "reviewerName": "SmilingSean",
+        "responseToReview": null,
+        "helpfulness": 42
+    },
+    {
+        "rating": 3,
+        "date": "2021-03-08T00:00:00.000Z",
+        "summary": "Nice combo",
+        "body": "I like to combo this product with my monkey shoes for a +30 fashion and a bonus to my critical strike",
+        "photos": [
+            {
+                "id": 496601,
+                "url": "https://i.ibb.co/TBBWs1B/monkey-Shoes.jpg"
+            }
+        ],
+        "recommended": true,
+        "reviewerName": "MonkeyShoeMatt",
+        "responseToReview": null,
+        "helpfulness": 14
+    }]
+
+  const resp = {data: reviews};
+  axios.get.mockResolvedValue(resp);
+
+  return ReviewAPI.getReviews(13023)
+    .then(res => expect(res.data).toEqual(reviews))
+});
